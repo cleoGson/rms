191f9bb54865b9cc249c34b38c23f030638e2cd0
Frank <gfrankgef@gmail.com>
2019-09-07 17:41:27 +0300

    User Api generated

--- public/js/app.js
+++ public/js/app.js
@@ -41292,7 +41292,7 @@ var staticRenderFns = [
       _vm._v(" "),
       _c(
         "button",
-        { staticClass: "btn btn-primary blue", attrs: { type: "button" } },
+        { staticClass: "btn btn-primary blue", attrs: { type: "submit" } },
         [_vm._v("Create")]
       )
     ])
